b420b4eeceb8b96126def231db3f9be9c12e80a8
Monkey Juice <miaowm5@gmail.com>
2025-08-16 18:32:27 +0000

    character show title

--- src/list/index.svelte
+++ src/list/index.svelte
@@ -6,16 +6,23 @@
   const list = Object.keys(database)
 
   const getName = (data)=>{
-    if (data[4] === '0'){ return database2[data[5]][0] }
-    if (data[4] === '1'){ return database2[data[5]][0] }
-    return data[17]
+    if (data[4] === '0'){ return [database2[data[5]][0], database2[data[5]][3]] }
+    if (data[4] === '1'){ return [database2[data[5]][0], database2[data[5]][3]] }
+    return [data[17]]
   }
 </script>
+
+{#snippet item(name)}
+	<p>{name[0]}</p>
+  {#if name[1]}<p class="title">{name[1]}</p>{/if}
+{/snippet}
 
 <div class={`main ${store.state.ui.mobileListHide ? 'mobileHide' : ''}`}>
   {#each list as id}
     <Nav href={`/${id}`} route={store.route}>
-      <span class={`item ${store.state.item === id ? 'active' : ''}`}>{getName(database[id][0])}</span>
+      <div class={`item ${store.state.item === id ? 'active' : ''}`}>
+        {@render item(getName(database[id][0]))}
+      </div>
     </Nav>
   {/each}
 </div>
@@ -40,6 +47,9 @@
   .item.active{
     background-color: #ddd;
     cursor: default;
+  }
+  .title{
+    font-size: .85em;
   }
   @media (max-width: 800px) {
     .main{
