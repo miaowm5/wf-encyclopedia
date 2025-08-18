7115421a14da989da94c0dc279ae6133eb2785bb
Monkey Juice <miaowm5@gmail.com>
2025-08-18 13:57:20 +0000

    load character data

--- src/detail/characterTitle.svelte
+++ src/detail/characterTitle.svelte
@@ -1,6 +1,7 @@
 <script>
   import store from '../store'
   import NpcTitle from './npcTitle.svelte'
+  import { wrapApi } from './common'
 
   const { data } = $props()
 
@@ -11,16 +12,48 @@
     document.title = character[0] + ' | ' + import.meta.env.VITE_SITE_NAME
     return character
   })
+
+  let loadDBState = $state(false)
+  wrapApi(`/orderedmap/character/character.json`, {
+    before: ()=>{
+      if (store.state.database.character){
+        loadDBState = true
+        return false
+      }
+    },
+    success: (data)=>{
+      const pureData = {}
+      Object.keys(data).forEach((key)=>{ pureData[key] = data[key][0] })
+      store.setDatabase('character', pureData)
+      loadDBState = true
+    }
+  }, true)
+  const characterData = $derived.by(()=>{
+    if (!loadDBState) return null
+    return store.state.database.character[data[0][5]]
+  })
+
 </script>
 
-<NpcTitle
-  title={character[3]}
-  name={character[0]}
-  cv={character[11]}
-  element='(None)'
-  race='(None)'
-  gender='(None)'
-/>
+{#if characterData}
+  <NpcTitle
+    title={character[3]}
+    name={character[0]}
+    cv={character[11]}
+    element={characterData[3]}
+    race={characterData[4]}
+    gender={characterData[7]}
+  />
+{:else}
+  <NpcTitle
+    title={character[3]}
+    name={character[0]}
+    cv={character[11]}
+    element='(None)'
+    race='(None)'
+    gender='(None)'
+  />
+{/if}
 
 <style>
 </style>
