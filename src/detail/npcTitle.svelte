51b03b4e16764564b69f4c29220e196190835875
Monkey Juice <miaowm5@gmail.com>
2025-08-18 14:09:32 +0000

    show race text

--- src/detail/npcTitle.svelte
+++ src/detail/npcTitle.svelte
@@ -18,6 +18,22 @@
     return '(None)'
   })
 
+  const raceText = $derived.by(()=>{
+    if (race === '(None)'){ return race }
+    return race.split(',').map((item)=>{
+      if (item === 'Human'){ return '人' }
+      if (item === 'Element'){ return '精灵' }
+      if (item === 'Devil'){ return '魔' }
+      if (item === 'Beast'){ return '兽' }
+      if (item === 'Machine'){ return '机械' }
+      if (item === 'Mystery'){ return '妖' }
+      if (item === 'Dragon'){ return '龙' }
+      if (item === 'Undead'){ return '不死' }
+      if (item === 'Aquatic'){ return '水棲' }
+      if (item === 'Plants'){ return '植物' }
+      return item
+    })
+  })
 </script>
 
 <div class="character">
@@ -29,7 +45,7 @@
       {title}
     </p>{/if}
     <p class="name">{name}</p>
-    {#if race !== '(None)'}<p>种族：{race}</p>{/if}
+    {#if raceText !== '(None)'}<p>种族：{raceText.join('/')}</p>{/if}
     {#if gender !== '(None)'}<p>性别：{genderText}</p>{/if}
     {#if cv !== '(None)'}<p>CV：{cv}</p>{/if}
   </div>
