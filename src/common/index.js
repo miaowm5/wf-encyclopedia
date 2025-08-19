25126c9c80a72ff71009f3ba4202a8042e13cf14
Monkey Juice <miaowm5@gmail.com>
2025-08-19 05:35:43 +0000

    add spritesheet

--- src/common/index.js
+++ src/common/index.js
@@ -1,9 +1,10 @@
 
 import { api, EventEmitter, Route } from './m5api'
 import wrapApi from './wrapApi.svelte.js'
+import spriteSheet from './spriteSheet.svelte.js'
 import Nav from './nav.svelte'
 import OnBack from './onBack.svelte'
 
 export {
-  api, wrapApi, EventEmitter, Route, Nav, OnBack,
+  api, wrapApi, spriteSheet, EventEmitter, Route, Nav, OnBack,
 }
---
+++ src/common/spriteSheet.svelte.js
@@ -0,0 +1,59 @@
+import { onDestroy, onMount } from "svelte"
+import { api } from './m5api'
+
+const cache = {}
+const cacheImage = {}
+
+const loadConfig = async (spritesheet)=>{
+  if (cache[spritesheet]){ return cache[spritesheet] }
+  const url = `${import.meta.env.VITE_CDN}${spritesheet}.json`
+  const result = await new Promise((resolve)=>{
+    api(url, { success: (data)=>{ resolve(data) } })
+  })
+  if (result){ cache[spritesheet] = result }
+  return result
+}
+const loadImage = async (spritesheet, file)=>{
+  const key = `${spritesheet}/${file}`
+  if (cacheImage[key]){ return cacheImage[key] }
+  const image = await new Promise((resolve)=>{
+    const img = new Image()
+    img.crossOrigin = "anonymous"
+    img.src = `${import.meta.env.VITE_CDN}${key}`
+    img.onload = ()=>{ resolve(img) }
+    img.onerror = ()=>{ resolve(null) }
+  })
+  if (image){ cacheImage[key] = image }
+  return image
+}
+
+const wrap = (spritesheet, file)=>{
+  let cancelFunc = false
+  onDestroy(()=>{ cancelFunc = true })
+  let src = $state('')
+  const load = async ()=>{
+    const sheetConfig = await loadConfig(spritesheet)
+    if (cancelFunc){ return }
+    const spriteConfig = sheetConfig[file + '.png']
+    if (!spriteConfig){ return }
+    const image = await loadImage(spritesheet, spriteConfig.image)
+    if (!image){ return }
+    if (cancelFunc){ return }
+    const canvas = document.createElement("canvas")
+    const ctx = canvas.getContext("2d")
+    canvas.width = spriteConfig.sourceSize.w
+    canvas.height = spriteConfig.sourceSize.h
+    ctx.drawImage(image,
+      spriteConfig.frame.x, spriteConfig.frame.y,
+      spriteConfig.frame.w, spriteConfig.frame.h,
+      spriteConfig.spriteSourceSize.x, spriteConfig.spriteSourceSize.y,
+      spriteConfig.frame.w, spriteConfig.frame.h,
+    )
+    src = canvas.toDataURL("image/png")
+  }
+  onMount(()=>{ load() })
+
+  return { get src(){ return src } }
+}
+
+export default wrap
