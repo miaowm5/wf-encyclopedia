a7cf982110e4cf1ccbc239f47f5af8619f428711
Monkey Juice <miaowm5@gmail.com>
2025-08-20 12:11:45 +0000

    load character data when init

--- src/detail/characterTitle.svelte
+++ src/detail/characterTitle.svelte
@@ -1,11 +1,11 @@
 <script>
   import store from '../store'
   import NpcTitle from './npcTitle.svelte'
-  import { wrapApi } from '../common'
 
   const { data } = $props()
 
   const database2 = store.state.database.character_text
+  const database3 = store.state.database.character
 
   const character = $derived.by(()=>{
     const character = database2[data[0][5]]
@@ -13,24 +13,8 @@
     return character
   })
 
-  let loadDBState = $state(false)
-  wrapApi(`/orderedmap/character/character.json`, {
-    before: ()=>{
-      if (store.state.database.character){
-        loadDBState = true
-        return false
-      }
-    },
-    success: (data)=>{
-      const pureData = {}
-      Object.keys(data).forEach((key)=>{ pureData[key] = data[key][0] })
-      store.setDatabase('character', pureData)
-      loadDBState = true
-    }
-  }, true)
   const characterData = $derived.by(()=>{
-    if (!loadDBState) return null
-    return store.state.database.character[data[0][5]]
+    return database3[data[0][5]]
   })
 
 </script>
