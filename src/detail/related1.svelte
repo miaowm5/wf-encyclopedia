5169b5fe3f56afa7ca64ac1ea2232c45679be162
Monkey Juice <miaowm5@gmail.com>
2025-08-21 16:49:35 +0000

    show head icon

--- src/detail/related1.svelte
+++ src/detail/related1.svelte
@@ -1,12 +1,13 @@
 <script>
   import store from '../store'
-  import { Nav } from '../common'
+  import { Nav, HeadIcon } from '../common'
   import Title from './title.svelte'
 
   const { data } = $props()
 
   const database = store.state.database.encyclopedia
   const database2 = store.state.database.character_text
+  const database3 = store.state.database.character
 
   const list = $derived.by(()=>{
     let result = []
@@ -15,9 +16,9 @@
       if (!item){ return }
       let character = null
       if (item[0][4] === '0' || item[0][4] === '1'){
-        character = { name: database2[item[0][5]][0] }
+        character = { name: database2[item[0][5]][0], head: database3[item[0][5]][0] }
       }else if (item[0][4] === '2'){
-        character = { name: item[0][17] }
+        character = { name: item[0][17], head: item[0][6] }
       }else{ return }
       result.push({character, id})
     })
@@ -33,7 +34,9 @@
     {#each list as item}
       <span class="tag">
         <Nav href={`/${item.id}`} route={store.route}>
-          {item.character.name}
+          <div class="border1"><div class="border2">
+            <HeadIcon name={item.character.name} file={item.character.head} />
+          </div></div>
         </Nav>
       </span>
     {/each}
@@ -48,16 +51,19 @@
     gap: .7em .5em;
     justify-content: center;
   }
-  .tag {
-    font-size: .9em;
-    display: inline-flex;
-    align-items: center;
-    padding: .2em 1em;
-    border-radius: 999px;
-    background: #fafafa;
-    color: #2bcaba;
-    white-space: nowrap;
-    border-bottom: 2px solid #a4a4a4;
-    border-top: 1px solid white;
+  .tag{
+    max-width: 17%;
+    position: relative;
+    line-height: 0;
+    border: 1px solid #ababab;
+    border-radius: 5px;
+    overflow: hidden;
+  }
+  .border2{
+    border: 1px solid #ababab;
+    background-color: #232223;
+  }
+  .border1{
+    border: 4px solid white;
   }
 </style>
