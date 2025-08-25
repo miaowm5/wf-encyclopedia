85cc303f20e05685c70ee8b64df330ab5709f2dc
Monkey Juice <miaowm5@gmail.com>
2025-08-25 16:50:29 +0000

    fix error

--- src/common/headIcon.svelte.js
+++ src/common/headIcon.svelte.js
@@ -4,10 +4,10 @@ import textImage from './textImage.svelte.js'
 const wrap = (file, name)=>{
 
   const icon = $derived.by(()=>{
-    return spriteSheet('character/head2', file ? file : null)
+    return spriteSheet('character/head2', file ? file : null, 'canvas')
   })
   const src = $derived.by(()=>{
-    if (icon.src){ return icon.src }
+    if (icon.src){ return icon.src.toDataURL("image/png") }
     const canvas = textImage(name, 212, 212, { color: 'white', background: '#000000', size: '36px' })
     return canvas.toDataURL("image/png")
   })
