2558925b4c5feccaccc915c8ec071be9b2540dba
Monkey Juice <miaowm5@gmail.com>
2025-08-25 09:50:34 +0000

    change headIcon to method

--- src/detail/relatedHead.svelte
+++ src/detail/relatedHead.svelte
@@ -1,8 +1,25 @@
 <script>
-  import { HeadIcon } from '../common'
+  import { headIcon } from '../common'
 
   const { name, file } = $props()
 
+  const icon = $derived.by(()=>{
+    return headIcon(name, file)
+  })
+
 </script>
 
-<HeadIcon name={name} file={file} />
+<div class="border1"><div class="border2">
+  <img src={icon.src} alt={name} />
+</div></div>
+
+<style>
+  .border2{
+    border: 1px solid #ababab;
+    background-color: #232223;
+  }
+  .border1{
+    border: 4px solid white;
+  }
+  img{ width: 100% }
+</style>
