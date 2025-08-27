eaaee6c5c229a3fc99783f8af9d463a7f0bace29
Monkey Juice <miaowm5@gmail.com>
2025-08-27 12:06:22 +0000

    story item

--- src/list/storyList.svelte
+++ src/list/storyList.svelte
@@ -1,35 +1,15 @@
 <script>
   import { Nav } from '../common'
   import store from '../store'
+  import StoryItem from './storyItem.svelte'
   const { list, select } = $props()
   const showList = $derived.by(()=>{
     return list.filter((data)=>data.item[4] === '3' || data.item[4] === '4')
   })
 </script>
 
-{#each showList as data}
+{#each showList as data}{#key data.id}
   <Nav href={`/${data.id}`} route={store.route}>
-    <div class={`item ${select === data.id ? 'active' : ''}`}>
-      <p>{data.name[0]}</p>
-    </div>
+    <StoryItem data={data} active={select === data.id} />
   </Nav>
-{/each}
-
-<style>
-  .item{
-    background: #fafafa;
-    border-top: 1px solid white;
-    display: block;
-    margin-bottom: .5em;
-    border-radius: 10px;
-    padding: .5em 1em;
-    box-shadow: 1px 1px 5px rgba(0,0,0,0.3);
-  }
-  .item:hover{
-    background-color: #d3efec;
-  }
-  .item.active{
-    background-color: #d3efec;
-    cursor: default;
-  }
-</style>
+{/key}{/each}
