c6b2bf53c96f263fbcd37c0209fa1f94246ccd7e
Monkey Juice <miaowm5@gmail.com>
2025-08-27 06:31:15 +0000

    basic list category

---
+++ src/list/allList.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { Nav } from '../common'
+  import store from '../store'
+
+  const { list, select } = $props()
+</script>
+
+{#each list as data}
+  <Nav href={`/${data.id}`} route={store.route}>
+    <div class={`item ${select === data.id ? 'active' : ''}`}>
+      <p>{data.name[0]}</p>
+      {#if data.name[1]}<p class="title">{data.name[1]}</p>{/if}
+    </div>
+  </Nav>
+{/each}
+
+<style>
+  .item{
+    background: #fafafa;
+    border-top: 1px solid white;
+    display: block;
+    margin-bottom: .5em;
+    border-radius: 10px;
+    padding: .5em 1em;
+    box-shadow: 1px 1px 5px rgba(0,0,0,0.3);
+  }
+  .item:hover{
+    background-color: #d3efec;
+  }
+  .item.active{
+    background-color: #d3efec;
+    cursor: default;
+  }
+  .title{
+    font-size: .85em;
+  }
+</style>
---
+++ src/list/storyList.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { Nav } from '../common'
+  import store from '../store'
+  const { list, select } = $props()
+  const showList = $derived.by(()=>{
+    return list.filter((data)=>data.item[4] === '3' || data.item[4] === '4')
+  })
+</script>
+
+{#each showList as data}
+  <Nav href={`/${data.id}`} route={store.route}>
+    <div class={`item ${select === data.id ? 'active' : ''}`}>
+      <p>{data.name[0]}</p>
+    </div>
+  </Nav>
+{/each}
+
+<style>
+  .item{
+    background: #fafafa;
+    border-top: 1px solid white;
+    display: block;
+    margin-bottom: .5em;
+    border-radius: 10px;
+    padding: .5em 1em;
+    box-shadow: 1px 1px 5px rgba(0,0,0,0.3);
+  }
+  .item:hover{
+    background-color: #d3efec;
+  }
+  .item.active{
+    background-color: #d3efec;
+    cursor: default;
+  }
+</style>
