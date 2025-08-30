b9a4cce366b46adc8911d54e455e04c088ac8b80
Monkey Juice <miaowm5@gmail.com>
2025-08-30 13:56:39 +0000

    add list banner

--- src/list/characterList.svelte
+++ src/list/characterList.svelte
@@ -1,11 +1,21 @@
 <script>
   import { Nav } from '../common'
   import store from '../store'
+  import ListBanner from './listBanner.svelte'
   const { list, select } = $props()
   const showList = $derived.by(()=>{
     return list.filter((data)=>data.item[4] === '0' || data.item[4] === '1' || data.item[4] === '2')
   })
 </script>
+
+<ListBanner
+  back="keyword_list_character_header_background"
+  title="keyword_list_character_title"
+  >
+  <button class="filter">
+    <p>筛选</p>
+  </button>
+</ListBanner>
 
 {#each showList as data}
   <Nav href={`/${data.id}`} route={store.route}>
@@ -17,6 +27,17 @@
 {/each}
 
 <style>
+  .filter{
+    border-radius: 100%;
+    width: 3em;
+    height: 3em;
+    position: absolute;
+    left: .5em;
+    bottom: 1em
+  }
+  .filter>p{
+    font-size: .9em;
+  }
   .item{
     background: #fafafa;
     border-top: 1px solid white;
@@ -32,5 +53,8 @@
   .item.active{
     background-color: #d3efec;
     cursor: default;
+  }
+  .item>.title{
+    font-size: .85em;
   }
 </style>
---
+++ src/list/listBanner.svelte
@@ -0,0 +1,31 @@
+<script>
+  const { back, title, children, } = $props()
+  const urlBack = `url(${import.meta.env.VITE_CDN}ui/${back}.png)`
+  const urlTitle = `url(${import.meta.env.VITE_CDN}ui/${title}.png)`
+</script>
+
+<div class="header" style:background-image={urlBack}>
+  {#if title}<div class="title" style:background-image={urlTitle}></div>{/if}
+  {@render children?.()}
+</div>
+
+<style>
+  .header{
+    height: 556px;
+    max-height: 30%;
+    background-size: cover;
+    background-position: center;
+    position: relative;
+    margin-bottom: 2em;
+  }
+  .title{
+    position: absolute;
+    left: 30%;
+    right: 30%;
+    top: 0;
+    bottom: 0;
+    background-size: 100% auto;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+</style>
