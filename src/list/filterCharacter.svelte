cc7d519518d61ac9db6169f1223c8d28aec52a23
Monkey Juice <miaowm5@gmail.com>
2025-08-31 12:46:18 +0000

    update style

--- src/list/filterCharacter.svelte
+++ src/list/filterCharacter.svelte
@@ -41,7 +41,11 @@
 </script>
 
 {#snippet title(text)}
-  <p class="title">{text}</p>
+  <div class="title">
+    <div class="l" style:background-image={`url(${import.meta.env.VITE_CDN + 'ui/flipper_border_left.png'})`}></div>
+    <p>{text}</p>
+    <div class="r" style:background-image={`url(${import.meta.env.VITE_CDN + 'ui/flipper_border.png'})`}></div>
+  </div>
 {/snippet}
 
 {#snippet textButton(group, id, text)}
@@ -55,43 +59,45 @@
 
 <div class="main"><div class="dialog">
   <div class="header">筛选</div>
-  {@render title('稀有度')}
-  <div class="group">
-    {@render textButton('rarity', '5', '五')}
-    {@render textButton('rarity', '4', '四')}
-    {@render textButton('rarity', '3', '三')}
-    {@render textButton('rarity', '2', '二')}
-    {@render textButton('rarity', '1', '一')}
-    {@render textButton('rarity', '0', '其他')}
-  </div>
-  {@render title('属性')}
-  <div class="group">
-    {@render textButton('element', '0', '火')}
-    {@render textButton('element', '1', '水')}
-    {@render textButton('element', '2', '雷')}
-    {@render textButton('element', '3', '风')}
-    {@render textButton('element', '4', '光')}
-    {@render textButton('element', '5', '暗')}
-    {@render textButton('element', '-1', '其他')}
-  </div>
-  {@render title('性别')}
-  <div class="group">
-    {@render textButton('sex', 'Female', '女')}
-    {@render textButton('sex', 'Male', '男')}
-    {@render textButton('sex', 'Other', '其他')}
-  </div>
-  {@render title('种族')}
-  <div class="group">
-    {@render textButton('race', 'Human', '人')}
-    {@render textButton('race', 'Element', '精灵')}
-    {@render textButton('race', 'Devil', '魔')}
-    {@render textButton('race', 'Beast', '兽')}
-    {@render textButton('race', 'Machine', '机械')}
-    {@render textButton('race', 'Mystery', '妖')}
-    {@render textButton('race', 'Dragon', '龙')}
-    {@render textButton('race', 'Undead', '不死')}
-    {@render textButton('race', 'Aquatic', '水棲')}
-    {@render textButton('race', 'Plants', '植物')}
+  <div class="content">
+    {@render title('稀有度')}
+    <div class="group">
+      {@render textButton('rarity', '5', '五')}
+      {@render textButton('rarity', '4', '四')}
+      {@render textButton('rarity', '3', '三')}
+      {@render textButton('rarity', '2', '二')}
+      {@render textButton('rarity', '1', '一')}
+      {@render textButton('rarity', '0', '其他')}
+    </div>
+    {@render title('属性')}
+    <div class="group">
+      {@render textButton('element', '0', '火')}
+      {@render textButton('element', '1', '水')}
+      {@render textButton('element', '2', '雷')}
+      {@render textButton('element', '3', '风')}
+      {@render textButton('element', '4', '光')}
+      {@render textButton('element', '5', '暗')}
+      {@render textButton('element', '-1', '其他')}
+    </div>
+    {@render title('性别')}
+    <div class="group">
+      {@render textButton('sex', 'Female', '女')}
+      {@render textButton('sex', 'Male', '男')}
+      {@render textButton('sex', 'Other', '其他')}
+    </div>
+    {@render title('种族')}
+    <div class="group">
+      {@render textButton('race', 'Human', '人')}
+      {@render textButton('race', 'Element', '精灵')}
+      {@render textButton('race', 'Devil', '魔')}
+      {@render textButton('race', 'Beast', '兽')}
+      {@render textButton('race', 'Machine', '机械')}
+      {@render textButton('race', 'Mystery', '妖')}
+      {@render textButton('race', 'Dragon', '龙')}
+      {@render textButton('race', 'Undead', '不死')}
+      {@render textButton('race', 'Aquatic', '水棲')}
+      {@render textButton('race', 'Plants', '植物')}
+    </div>
   </div>
   <div class="submit">
     <button onclick={closeFilter}>取消</button>
@@ -114,13 +120,21 @@
   }
   .dialog{
     width: 80%;
+    max-width: 600px;
     max-height: 80%;
     background-color: #fafafa;
-    overflow: auto;
     border: 2px solid white;
     border-radius: 1em;
     box-shadow: 0 0 8px rgba(0,0,0,.3);
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
     padding: 1em;
+  }
+  .content{
+    flex: 1;
+    overflow: auto;
+    padding: 0 .5em 2em;
   }
   .header{
     text-align: center;
@@ -129,8 +143,19 @@
   }
   .title{
     text-align: center;
-    padding: .5em 0;
+    padding: 1em 0;
+    display: flex;
   }
+  .title>p{
+    padding: 0 .5em;
+  }
+  .title>div{
+    flex: 1;
+    background-size: auto 100%;
+    background-repeat: no-repeat;
+  }
+  .l{ background-position: right; }
+  .r{ background-position: left; }
   .group, .submit{
     display: flex;
     flex-wrap: wrap;
@@ -138,6 +163,7 @@
   }
   .group{
     gap: .7em .5em;
+    margin-bottom: .5em;
   }
   .choice{
     display: inline-flex;
@@ -151,8 +177,9 @@
     background-color: #ffcf8f;
   }
   .submit{
-    margin-top: 1.5em;
+    margin-top: 1em;
     gap: .5em 1em;
+    padding-bottom: .5em;
   }
   .submit>button{
     padding: .5em 3em;
