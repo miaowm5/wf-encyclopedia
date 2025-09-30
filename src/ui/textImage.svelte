53902e56acd948f20b81db70b9c8f2ac0e041f25
Monkey Juice <miaowm5@gmail.com>
2025-09-30 05:51:58 +0000

    add lazy load

--- src/ui/textImage.svelte
+++ src/ui/textImage.svelte
@@ -1,17 +1,27 @@
 <script>
   import { textImage } from "../common"
+  import lazyLoadWrap from './lazyLoad.svelte.js'
+
   let {
     text,
     width,
     height,
+    lazyLoad = true,
     style = {},
   } = $props()
+
+  let node = $state(null)
+  let lazyLoadStatus = lazyLoadWrap(()=>node, lazyLoad)
   const src = $derived.by(()=>{
+    if (!lazyLoadStatus.load){
+      return `data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7`
+    }
     return textImage(text, width, height, style).toDataURL("image/png")
   })
 </script>
 
-<img src={src} alt={text}>
+{#if !lazyLoadStatus.load}<span bind:this={node}></span>{/if}
+<img src={src} alt={text} width={width} height={height}>
 
 <style>
   img{
