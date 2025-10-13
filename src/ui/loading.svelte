d65240d9182918e576a5eb32c59971816f074ee1
Monkey Juice <miaowm5@gmail.com>
2025-10-13 09:29:26 +0000

    add loading

--- src/ui/loading.svelte
+++ src/ui/loading.svelte
@@ -1,1 +1,17 @@
-<p>Loading...</p>
+<script>
+
+  let {
+    finish = false,
+    error = [],
+    children,
+  } = $props()
+
+</script>
+
+{#if error.length > 0}
+  <p>{error[0]}</p>
+{:else if finish}
+  {@render children?.()}
+{:else}
+  <p>Loading...</p>
+{/if}
