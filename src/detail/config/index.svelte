2664dce30bb3b7ecf606f8168408f7131c1413fe
Monkey Juice <miaowm5@gmail.com>
2025-10-15 11:43:27 +0000

    update info

--- src/detail/config/index.svelte
+++ src/detail/config/index.svelte
@@ -17,20 +17,29 @@
 
 <div class="content">
   <button class="back" onclick={()=>{ store.route.push('/', true) }}>
-    <img src={backSprite.src} alt={store.i18n("detail.content.configBack")}>
-    {store.i18n("detail.content.configBack")}
+    <img src={backSprite.src} alt={store.i18n("detail.config.configBack")}>
+    {store.i18n("detail.config.configBack")}
   </button>
   <div class="main">
-    <Title>{store.i18n("detail.content.config1")}</Title>
+    <Title>{store.i18n("detail.config.config1")}</Title>
     <div class="btns">
-      {@render button(store.i18n("detail.content.config1Value1"), 'dataRegion', 'cn')}
-      {@render button(store.i18n("detail.content.config1Value2"), 'dataRegion', 'jp')}
+      {@render button(store.i18n("detail.config.config1Value1"), 'dataRegion', 'cn')}
+      {@render button(store.i18n("detail.config.config1Value2"), 'dataRegion', 'jp')}
     </div>
-    <Title>{store.i18n("detail.content.config2")}</Title>
+    <Title>{store.i18n("detail.config.config2")}</Title>
     <div class="btns">
-      {@render button(store.i18n("detail.content.config2Value1"), 'language', 'cn')}
-      {@render button(store.i18n("detail.content.config2Value2"), 'language', 'jp')}
-      {@render button(store.i18n("detail.content.config2Value3"), 'language', 'en')}
+      {@render button(store.i18n("detail.config.config2Value1"), 'language', 'cn')}
+      {@render button(store.i18n("detail.config.config2Value2"), 'language', 'jp')}
+      {@render button(store.i18n("detail.config.config2Value3"), 'language', 'en')}
+    </div>
+    <Title>{store.i18n("detail.config.config3")}</Title>
+    <div class="desc">{#each store.i18n("detail.config.config3Value").split('\n') as line}
+      <p>{line}</p>
+    {/each}</div>
+    <div class="link">
+      <a href="https://github.com/miaowm5/wf-encyclopedia" target="_blank">{store.i18n("detail.config.config3Value1")}</a>
+      <a href="https://github.com/miaowm5/wf-encyclopedia/issues" target="_blank">{store.i18n("detail.config.config3Value2")}</a>
+      <a href="http://worldflipper.jp/" target="_blank">ワールドフリッパー公式サイト</a>
     </div>
   </div>
 </div>
@@ -68,7 +77,6 @@
     gap: .7em .5em;
     justify-content: center;
     margin: .5em auto 1em;
-
   }
   .btn{
     padding: .5em 2em;
@@ -81,5 +89,26 @@
     border-top: none;
     background-color: #ffcf8f;
   }
-
+  .desc{
+    display: block;
+    margin-bottom: .5em;
+    border-radius: 10px;
+    padding: 1em;
+    background-color: white;
+  }
+  .desc>p{
+    min-height: 1em;
+  }
+  .link > a{
+    background: #fafafa;
+    box-shadow: 1px 1px 5px rgba(0,0,0,0.3);
+    cursor: pointer;
+    color: #444444;
+    text-decoration: none;
+    display: block;
+    padding: .5em 1em;
+    border-top: 1px solid white;
+    border-radius: 10px;
+    margin-bottom: .5em
+  }
 </style>
