023b0bd85418bf0036b2a093e27ebcbcf679340b
Monkey Juice <miaowm5@gmail.com>
2025-11-21 13:11:28 +0000

    update mini player style

--- src/list/musicPlayer/miniPlayer.svelte
+++ src/list/musicPlayer/miniPlayer.svelte
@@ -1,21 +1,32 @@
 <script>
   import store from '../../store'
+  import { spriteSheet } from '../../common'
   const { songName } = $props()
+
+  const playMarker = spriteSheet('res/icon', 'voice_volume3')
+  const pauseMarker = spriteSheet('res/icon', 'option_small')
+  const listMarker = spriteSheet('res/icon', 'voice_volume3')
 </script>
 
 <div class="main">
-  <button onclick={()=>{
+  <button class="song" onclick={()=>{
     if (!songName[0]){ return }
     if (store.state.jukebox.playing){ store.jukebox.pause() }
     else{ store.jukebox.play() }
   }}>
     {#if songName[0]}
-      <span>{store.state.jukebox.playing ? store.i18n("暂停") : store.i18n("播放")}</span>
+      <span class="icon">
+        {#if store.state.jukebox.playing}
+          <img class="icon" src={pauseMarker.src} alt={store.i18n("detail.music.pause")}>
+        {:else}
+          <img class="icon" src={playMarker.src} alt={store.i18n("detail.music.play")}>
+        {/if}
+      </span>
     {/if}
     <span>{songName[0]}</span>
   </button>
-  <button onclick={()=>{ store.triggerJukebox(true) }}>
-    <span>{store.i18n("列表")}</span>
+  <button class="list" onclick={()=>{ store.triggerJukebox(true) }}>
+    <span class="icon"><img src={listMarker.src} alt={store.i18n("detail.music.playlist")}></span>
   </button>
 </div>
 
@@ -23,7 +34,7 @@
 <style>
   .main{
     display: flex;
-    padding: .5em 1em;
+    padding: .5em 0;
     background-color: #fafafa;
     border-top: 1px solid #ddd;
   }
@@ -39,17 +50,30 @@
     min-width: 0;
     overflow: hidden;
     display: flex;
+    align-items: center;
   }
-  .main>button>span{
+  .song>span{
     white-space: nowrap;
   }
-  .main>button:nth-child(1)>span:nth-child(1){
+  .song>span:nth-child(1){
     flex: none;
   }
-  .main>button:nth-child(1)>span:nth-child(2){
+  .song>span:nth-child(2){
     flex: 1 1 0;
     min-width: 0;
     overflow: hidden;
     text-overflow: ellipsis;
+  }
+  .icon{
+    display: block;
+    height: 2em;
+    width: 2em;
+  }
+  .icon>img{
+    max-width: 100%;
+    max-height: 100%;
+  }
+  .main>button:nth-child(2){
+    border-left: 1px solid #ddd;
   }
 </style>
