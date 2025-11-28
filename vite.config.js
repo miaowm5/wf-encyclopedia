96a876d49007209f964d57f292767c217f1fa98e
Monkey Juice <miaowm5@gmail.com>
2025-11-28 18:33:44 +0800

    update cache rule

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { VitePWA } from 'vite-plugin-pwa'
 
-const proxy = 'https://worldflipper.miaowm5.com'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -39,15 +37,19 @@ export default defineConfig({
               expiration: { maxAgeSeconds: 60 * 60 * 24 * 30 },
               cacheableResponse: { statuses: [0, 200], },
             },
+          },
+          {
+            urlPattern: /^https:\/\/cdn\.miaowm5\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'cdn',
+              expiration: { maxAgeSeconds: 60 * 60 * 24 * 30 },
+              cacheableResponse: { statuses: [0, 200], },
+            },
           }
         ],
       },
     }),
   ],
   build: { emptyOutDir: true },
-  server: {
-    proxy: {
-      // '/api': { target: proxy, changeOrigin: true },
-    }
-  }
 })
