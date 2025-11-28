3c2417d8e4a404ede1761c674ff3064081298db1
Monkey Juice <miaowm5@gmail.com>
2025-11-28 19:05:03 +0800

    update cache rule

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,23 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const cacheOption = {
+  handler: 'CacheFirst',
+  options: {
+    cacheName: 'cdn',
+    expiration: { maxAgeSeconds: 60 * 60 * 24 * 30 },
+    cacheableResponse: { statuses: [0, 200], },
+  },
+}
+const cacheOption2 = {
+  handler: 'StaleWhileRevalidate',
+  options: {
+    cacheName: 'cdn',
+    expiration: { maxAgeSeconds: 60 * 60 * 24 * 30 },
+    cacheableResponse: { statuses: [0, 200], },
+  },
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,41 +29,37 @@ export default defineConfig({
       workbox: {
         runtimeCaching: [
           {
-            urlPattern: /^https:\/\/worldflipper-cdn\.miaowm5\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'cdn',
-              expiration: { maxAgeSeconds: 60 * 60 * 24 * 30 },
-              cacheableResponse: { statuses: [0, 200], },
-            },
+            urlPattern: /^https:\/\/worldflipper-cdn\.miaowm5\.com\/.*\.(png|jpe?g|gif|webp|bmp|svg|mp3|wav|ogg|m4a|flac)$/i,
+            ...cacheOption,
           },
           {
-            urlPattern: /^https:\/\/worldflipper-cdn2\.miaowm5\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'cdn',
-              expiration: { maxAgeSeconds: 60 * 60 * 24 * 30 },
-              cacheableResponse: { statuses: [0, 200], },
-            },
+            urlPattern: /^https:\/\/worldflipper-cdn2\.miaowm5\.com\/.*\.(png|jpe?g|gif|webp|bmp|svg|mp3|wav|ogg|m4a|flac)$/i,
+            ...cacheOption,
           },
           {
-            urlPattern: /^https:\/\/worldflipper-cdn3\.miaowm5\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'cdn',
-              expiration: { maxAgeSeconds: 60 * 60 * 24 * 30 },
-              cacheableResponse: { statuses: [0, 200], },
-            },
+            urlPattern: /^https:\/\/worldflipper-cdn3\.miaowm5\.com\/.*\.(png|jpe?g|gif|webp|bmp|svg|mp3|wav|ogg|m4a|flac)$/i,
+            ...cacheOption,
           },
           {
-            urlPattern: /^https:\/\/cdn\.miaowm5\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'cdn',
-              expiration: { maxAgeSeconds: 60 * 60 * 24 * 30 },
-              cacheableResponse: { statuses: [0, 200], },
-            },
-          }
+            urlPattern: /^https:\/\/cdn\.miaowm5\.com\/.*\.(png|jpe?g|gif|webp|bmp|svg|mp3|wav|ogg|m4a|flac)$/i,
+            ...cacheOption,
+          },
+          {
+            urlPattern: /^https:\/\/worldflipper-cdn\.miaowm5\.com\/.*\.(json)$/i,
+            ...cacheOption2,
+          },
+          {
+            urlPattern: /^https:\/\/worldflipper-cdn2\.miaowm5\.com\/.*\.(json)$/i,
+            ...cacheOption2,
+          },
+          {
+            urlPattern: /^https:\/\/worldflipper-cdn3\.miaowm5\.com\/.*\.(json)$/i,
+            ...cacheOption2,
+          },
+          {
+            urlPattern: /^https:\/\/cdn\.miaowm5\.com\/.*\.(json)$/i,
+            ...cacheOption2,
+          },
         ],
       },
     }),
