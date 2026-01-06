d2450f24ee98b74b68dc8119194e1ac0ecbe0d42
Monkey Juice <miaowm5@gmail.com>
2026-01-06 15:48:44 +0000

    clean code

--- src/common/m5api.js
+++ src/common/m5api.js
@@ -1,13 +1,11 @@
 
 import RouteParser from 'route-parser'
 import apiFactory from 'm5api/svelte'
-import { onMount } from "svelte"
 
 const m5api = apiFactory({
   auth: undefined,
   api: '',
   route: RouteParser,
-  svelte: { onMount },
 })
 const api = m5api.api
 const EventEmitter = m5api.EventEmitter
