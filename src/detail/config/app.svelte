bb42c7f6ed09380f671daa5c07556207c4de1552
Monkey Juice <miaowm5@gmail.com>
2026-03-03 06:18:45 +0000

    tree shaking

--- src/detail/config/app.svelte
+++ src/detail/config/app.svelte
@@ -2,25 +2,14 @@
   import { cdn } from '../../common'
   import { Title } from '../../ui'
 
-  const { mode } = $props()
-
-  const getDeviceType = ()=>{
-    const ua = navigator.userAgent;
-    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)){
-      return "tablet"
-    }
-    if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)){
-      return "mobile"
-    }
-    return "desktop"
-  }
-
   const getCDNInfo = ()=>{
     const list = ['cdn', 'cdn2', 'cdn3', 'cdn4']
     const useable = list.map((name)=>!cdn(name).startsWith('http'))
     const allSet = useable.every(v=>v)
     return { list, useable, allSet }
   }
+
+  const info = getCDNInfo()
 </script>
 
 {#snippet appCDNInfo(name, offline)}
@@ -34,21 +23,11 @@
   </div>
 {/snippet}
 
-{#if mode === 'web'}
-  {#if getDeviceType() === 'desktop'}
-    <Title>PC Version</Title>
-    <a class="btn" href="/app/StarEncyclopedia-release.zip" download="StarEncyclopedia.zip">Download</a>
-  {/if}
-{:else if mode === 'app'}
-  <Title>CDN Status</Title>
-  {@const info = getCDNInfo()}
-  {#each info.list as item, index}{@render appCDNInfo(item, info.useable[index])}{/each}
-  {#if !info.allSet}
-    <p>Download cdn file to enable offline function.</p>
-    <button class="btn" onclick={()=>Neutralino.os.open('https://drive.google.com/drive/folders/10efVwEqhIKQWNRJBeJs1QxfrpWR7pDB7')}>Google Drive</button>
-  {/if}
-{:else if mode === 'pwa'}
-  <!-- skip -->
+<Title>CDN Status</Title>
+{#each info.list as item, index}{@render appCDNInfo(item, info.useable[index])}{/each}
+{#if !info.allSet}
+  <p>Download cdn file to enable offline function.</p>
+  <button class="btn" onclick={()=>Neutralino.os.open('https://drive.google.com/drive/folders/10efVwEqhIKQWNRJBeJs1QxfrpWR7pDB7')}>Google Drive</button>
 {/if}
 
 <style>
