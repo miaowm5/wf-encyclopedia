fbf6c187494571208988b363ba874dd9889b8882
Monkey Juice <miaowm5@gmail.com>
2026-05-13 11:10:03 +0800

    fix url

--- src/detail/config/app.svelte
+++ src/detail/config/app.svelte
@@ -22,7 +22,7 @@
 {#each info.list as item, index}{@render appCDNInfo(item, info.useable[index])}{/each}
 {#if !info.allSet}
   <p>Download cdn file to enable offline function.</p>
-  <button class="btn" onclick={()=>Neutralino.os.open('http://worldflipper.miaowm5.com/app/driver')}>Google Drive</button>
+  <button class="btn" onclick={()=>Neutralino.os.open('https://worldflipper.miaowm5.com/app/driver')}>Google Drive</button>
 {/if}
 
 <style>
