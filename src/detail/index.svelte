36afe1d0a425431d13f1c8748cdb14a05e29c73c
Monkey Juice <miaowm5@gmail.com>
2026-05-13 17:36:27 +0000

    update circle style

--- src/detail/index.svelte
+++ src/detail/index.svelte
@@ -1,5 +1,6 @@
 <script>
   import store from '../store'
+  import { cdn } from '../common'
   import { Loading } from '../ui'
   import extarGallery from '../database/extraGallery.json'
   import Title from './title/index.svelte'
@@ -128,13 +129,15 @@
     <Music />
   {/if}
   {#if store.state.item}{#key store.state.item}
-    <span class={[store.state.ui.page !== 'item' && 'hide', 'body']}>
-      <Loading finish={loadDB.finish} error={loadDB.error}>
-        {#if item}
-          <Title item={item} itemType={item.type} tab={tab === 'voice-none' ? 'voice' : tab} extra={store.state.extra} />
-          <Content item={item} itemType={item.type} tab={tab} extra={store.state.extra} />
-        {/if}
-      </Loading>
+    <span class={[store.state.ui.page !== 'item' && 'hide', 'body']} style:--magic-circle={`url('${cdn('cdn', 'ui/circle.png')}')`}>
+      <span>
+        <Loading finish={loadDB.finish} error={loadDB.error}>
+          {#if item}
+            <Title item={item} itemType={item.type} tab={tab === 'voice-none' ? 'voice' : tab} extra={store.state.extra} />
+            <Content item={item} itemType={item.type} tab={tab} extra={store.state.extra} />
+          {/if}
+        </Loading>
+      </span>
     </span>
   {/key}{/if}
 </div>
@@ -149,7 +152,36 @@
     display: flex;
     flex-direction: column;
     position: relative;
+    z-index: 0;
+    --magic-circle-pos: 60%;
   }
+  .body>span{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    z-index: 1;
+  }
+  .body::after {
+    z-index: -1;
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translate(-50%, var(--magic-circle-pos));
+    width: 70%;
+    height: 70%;
+    background: var(--magic-circle) no-repeat center/contain;
+    animation: rotate-magic 25s linear infinite;
+    pointer-events: none;
+  }
+  @keyframes rotate-magic {
+    from { transform: translate(-50%, var(--magic-circle-pos)) rotate(0deg); }
+    to { transform: translate(-50%, var(--magic-circle-pos)) rotate(360deg); }
+  }
+  @media (max-width: 800px){ .body { --magic-circle-pos: 60%; } }
+  @media (max-width: 350px){ .body { --magic-circle-pos: 55%; } }
+
   .hide{
     display: none;
   }
