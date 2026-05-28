2f92c2c5107a3ee969a32900df1ee84ebcccd23f
Monkey Juice <miaowm5@gmail.com>
2026-05-28 07:00:35 +0000

    update image loader logic

--- src/ui/imageLoader.svelte
+++ src/ui/imageLoader.svelte
@@ -2,45 +2,21 @@
   import LazyLoad from './lazyLoad.svelte'
 
   let { src, alt, children, lazyLoad = true } = $props()
-
-  let realSrc = $state(null)
-  let lazyLoadStatus = $state(false)
-
-  const load = (src)=>{
-    if (!src){ return }
-    let destory = false
-    const img = new Image()
-    img.crossOrigin = "anonymous"
-    img.src = src
-    img.onload = ()=>{
-      if (destory){ return }
-      const canvas = document.createElement("canvas")
-      const ctx = canvas.getContext("2d")
-      canvas.width = img.naturalWidth
-      canvas.height = img.naturalHeight
-      ctx.drawImage(img, 0, 0)
-      realSrc = canvas.toDataURL("image/png")
-    }
-    return ()=>{ destory = true }
-  }
-
-  $effect(()=>{
-    if (!lazyLoadStatus){ return }
-    return load(src)
-  })
+  let loadOver = $state(false)
 </script>
 
-{#if realSrc}
-  <img src={realSrc} alt={alt ? alt : src}>
-{:else}
-  <LazyLoad lazy={lazyLoad} load={()=>lazyLoadStatus = true}>
-    {@render children?.()}
-  </LazyLoad>
-{/if}
+<LazyLoad lazy={lazyLoad}>
+  {@render children?.()}
+  {#snippet loadChildren()}
+    {#if !loadOver}{@render children?.()}{/if}
+    <img src={src} alt={alt ? alt : src} onload={()=>loadOver=true} class:loading={!loadOver}>
+  {/snippet}
+</LazyLoad>
 
 <style>
   img{
     max-width: 100%;
     max-height: 100%;
   }
+  .loading{ display: none; }
 </style>
