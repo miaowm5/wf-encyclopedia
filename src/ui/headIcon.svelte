447f15ff21051e5a733c5dd6b1cb254361d291bc
Monkey Juice <miaowm5@gmail.com>
2026-05-29 08:55:50 +0000

    update headicon logic

--- src/ui/headIcon.svelte
+++ src/ui/headIcon.svelte
@@ -15,49 +15,40 @@
 
   let lazyLoadStatus = $state(false)
 
-  const spriteHead = $derived.by(()=>{
-    if (!lazyLoadStatus){ return null }
-    return spriteSheet('head', file, 'cdn2')
-  })
-  const spriteRarity = $derived.by(()=>{
-    if (!rarity || !lazyLoadStatus){ return null }
-    const file = {
-      '5': 'rarity_five',
-      '4': 'rarity_four',
-      '3': 'rarity_three',
-      '2': 'rarity_two',
-      '1': 'rarity_one',
-    }[rarity]
-    if (!file){ return null }
-    return spriteSheet('res/icon', file, 'cdn', headIconCache)
-  })
-  const spriteRarityFrame = $derived.by(()=>{
-    if (!rarity || !lazyLoadStatus){ return null }
-    return spriteSheet('res/icon', `rarity_background${rarity}`, 'cdn', headIconCache)
-  })
-  const spriteElement = $derived.by(()=>{
-    if (!element || !lazyLoadStatus){ return null }
-    const file = {
-      '0': 'element_red_medium',
-      '1': 'element_blue_medium',
-      '2': 'element_yellow_medium',
-      '3': 'element_green_medium',
-      '4': 'element_white_medium',
-      '5': 'element_black_medium',
-    }[element]
-    if (!file){ return null }
-    return spriteSheet('res/icon', file, 'cdn', headIconCache)
-  })
-  const spriteElementFrame = $derived.by(()=>{
-    if (!lazyLoadStatus){ return null }
-    if (!element){
-      return spriteSheet('res/icon', 'character_face_empty_frame', 'cdn', headIconCache)
-    }
-    return spriteSheet('res/icon', 'character_face_frame', 'cdn', headIconCache)
-  })
+  let spriteHead = $state(null)
+  let spriteRarity = $state(null)
+  let spriteRarityFrame = $state(null)
+  let spriteElement = $state(null)
+  let spriteElementFrame = $state(null)
+  const loadSprite = ()=>{
+    spriteHead = spriteSheet('head', ()=>file, 'cdn2')
+    spriteRarity = spriteSheet('res/icon', ()=>{
+      return {
+        '5': 'rarity_five',
+        '4': 'rarity_four',
+        '3': 'rarity_three',
+        '2': 'rarity_two',
+        '1': 'rarity_one',
+      }[rarity]
+    }, 'cdn', headIconCache)
+    spriteRarityFrame = spriteSheet('res/icon', ()=>rarity ? `rarity_background${rarity}` : null, 'cdn', headIconCache)
+    spriteElement = spriteSheet('res/icon', ()=> {
+      return {
+        '0': 'element_red_medium',
+        '1': 'element_blue_medium',
+        '2': 'element_yellow_medium',
+        '3': 'element_green_medium',
+        '4': 'element_white_medium',
+        '5': 'element_black_medium',
+      }[element]
+    }, 'cdn', headIconCache)
+    spriteElementFrame = spriteSheet(
+      'res/icon', ()=>`character_face_${element ? 'frame' : 'empty_frame'}`,
+      'cdn', headIconCache,
+    )
+  }
   let finalHead = $state(null)
   $effect(()=>{
-    if (!lazyLoadStatus){ finalHead = null; return }
     if (!spriteHead?.canvas){ finalHead = null; return }
     const canvas = document.createElement("canvas")
     const ctx = canvas.getContext("2d")
@@ -97,7 +88,7 @@
 </script>
 
 {#if !lazyLoadStatus}
-  <LazyLoad lazy={lazyLoad} load={()=>{ lazyLoadStatus = true }} lazyTime={200}>
+  <LazyLoad lazy={lazyLoad} load={()=>{ lazyLoadStatus = true; loadSprite() }} lazyTime={200}>
     <img class="frame" alt={name}
       src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
       style:aspect-ratio={showName ? "212/252" : "1/1"} />
@@ -111,12 +102,12 @@
     <TextImage
       text={name}
       width={212}
-      height={212}
+      height={showName ? 212 : 252}
       style={{ color: 'white', background: '#000000', size: '36px' }}
       lazyLoad={false}
     />
   {/if}
-  {#if showName}
+  {#if showName && finalHead}
     <TextImage
       text={name}
       width={212}
