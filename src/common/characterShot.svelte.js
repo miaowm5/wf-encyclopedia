32f35ff829809e990f5756ec7f850eda4d9c2769
Monkey Juice <miaowm5@gmail.com>
2026-05-29 13:32:40 +0000

    cache charactershot default value

--- src/common/characterShot.svelte.js
+++ src/common/characterShot.svelte.js
@@ -1,20 +1,22 @@
 import spriteSheet from './spriteSheet.svelte.js'
 
-const emptyCanvas = ()=>{
+const emptyCanvas = (width = 1, height = 1)=>{
   const canvas = document.createElement("canvas")
-  canvas.width = 1
-  canvas.height = 1
+  canvas.width = width
+  canvas.height = height
   return { get canvas(){ return canvas } }
 }
+const empty = emptyCanvas(1, 1)
+let srcCache = null
 
 const wrap = (back, front, effect=[], cache)=>{
 
   const backCanvas = $derived.by(()=>{
-    if (!back){ return emptyCanvas() }
+    if (!back){ return empty }
     return spriteSheet('character/story', back, 'cdn', cache)
   })
   const frontCanvas = $derived.by(()=>{
-    if (!front){ return emptyCanvas() }
+    if (!front){ return empty }
     return spriteSheet('character/story', front, 'cdn', cache)
   })
   const effectCanvas = $derived.by(()=>{
@@ -23,18 +25,20 @@ const wrap = (back, front, effect=[], cache)=>{
     })
   })
 
-  const src = $derived.by(()=>{
-    const canvas = document.createElement("canvas")
-    const ctx = canvas.getContext("2d")
-    canvas.width = 570
-    canvas.height = 690
+  const canvas = $derived.by(()=>{
     if (backCanvas.canvas && frontCanvas.canvas && effectCanvas.every(item => item.canvas !== null)){
+      const canvas = emptyCanvas(570, 690).canvas
+      const ctx = canvas.getContext("2d")
       ctx.drawImage(backCanvas.canvas, 0, 0)
       ctx.drawImage(frontCanvas.canvas, 0, 0)
       effectCanvas.forEach((eff)=>{ ctx.drawImage(eff.canvas, 0, 0) })
+      return canvas
     }
-    return canvas.toDataURL("image/png")
+    if (!srcCache){ srcCache = emptyCanvas(570, 690).canvas.toDataURL("image/png") }
+    return null
+
   })
+  const src = $derived(canvas ? canvas.toDataURL("image/png") : srcCache)
 
   return { get src(){ return src } }
 }
